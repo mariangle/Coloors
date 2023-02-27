1cf7bbf70232ab2e47551fb4cef3a9ec90e7635d
Maria <mariale171001@gmail.com>
2023-02-27 23:29:47 +0100

    fixed sliders

--- app.js
+++ app.js
@@ -22,7 +22,16 @@ class ColorPicker{
             // ADD COLOR TO BACKGROUND
             div.style.backgroundColor = randomColor;
             hexText.innerText = randomColor;
+            // CHECK CONTRAST
             this.checkTextContrast(randomColor, hexText);
+            // INITIALIZE COLORIZE SLIDERS
+            const color = chroma(randomColor);
+            const sliders = div.querySelectorAll(".sliders input");
+            const hue = sliders[0];
+            const brightness = sliders[1];
+            const saturation = sliders[2];
+
+            this.colorizeSliders(color, hue, brightness, saturation);
         })
     }
     checkTextContrast(color,text){
@@ -34,6 +43,21 @@ class ColorPicker{
         } else{
             text.style.color = "white";
         }
+    }
+    colorizeSliders(color, hue, brightness, saturation){
+        // SCALE SATURATION
+        const noSat = color.set("hsl.s", 0);
+        const fullSat = color.set("hsl.s", 1);
+        const scaleSat = chroma.scale([noSat,color, fullSat]);
+
+        // SCALE BRIGHTNESS
+        const midBright = color.set('hsl.l', 0.5);
+        const scaleBright = chroma.scale(["black", midBright, "white"]);
+
+        // UPDATE INPUT OF COLORS
+        saturation.style.backgroundImage = `linear-gradient(to right,${scaleSat(0)},${scaleSat(1)} )`;
+        brightness.style.backgroundImage = `linear-gradient(to right,${scaleBright(0)},${scaleBright(0.5)},${scaleBright(1)})`;
+        hue.style.backgroundImage = `linear-gradient(to right, rgb(204,75,75),rgb(204,204,75),rgb(75,204,75),rgb(75,204,204),rgb(75,75,204),rgb(204,75,204),rgb(204,75,75))`;
     }
 }
 
